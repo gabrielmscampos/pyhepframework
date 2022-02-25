a6d180bb8a1cd8b0b2cd815564b435157adc62f6
Gabriel Moreira <gabrielmscampos@gmail.com>
2022-02-25 16:31:49 -0300

    add EventLoop class to handle event loop using dependency injection with Provider and EventHandler classes

---
+++ pyhepframework/evt_loop.py
@@ -0,0 +1,44 @@
+from typing import Union, Dict
+
+from injector import inject
+
+from .provider import cern, desy, local
+from .evt_handler import EventHandler
+
+try:
+    import tqdm
+    HAS_TQDM = True
+except ImportError:
+    HAS_TQDM = False
+
+
+class EventLoop:
+    @inject
+    def __init__(
+        self,
+        config: Dict,
+        provider: Union[cern.CERN, desy.DESY, local.LocalProvider],
+        evthandler: EventHandler,
+    ) -> None:
+        self.provider = provider
+        self.evt_handler = evthandler
+        self.config = config
+
+    def start(self):
+        """
+        Start event loop
+        """
+        dataset = self.provider.read_file(self.config.get("file_path"))
+        events = self.provider.parse_events(dataset, objects=self.config.get("objects"))
+
+        print("Number of events considered: ", len(events))
+
+        self.evt_handler.set_config(self.config)
+
+        if HAS_TQDM:
+            iter_obj = tqdm.tqdm(events)
+        else:
+            iter_obj = events
+
+        for event in iter_obj:
+            self.evt_handler.main(event)
