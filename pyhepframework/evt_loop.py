c3f0f87dff2be00ef8bd91f7a2f46e64d7d5c47c
Gabriel Moreira <gabrielmscampos@gmail.com>
2022-03-14 21:54:07 -0300

    update EvtLoop adding option to specify max number of events considered in event loop

--- pyhepframework/evt_loop.py
+++ pyhepframework/evt_loop.py
@@ -7,6 +7,7 @@ from .evt_handler import EventHandler
 
 try:
     import tqdm
+
     HAS_TQDM = True
 except ImportError:
     HAS_TQDM = False
@@ -25,18 +26,54 @@ class EventLoop:
         self.config = config
         self.__output = []
 
+    @staticmethod
+    def __validate_max_entries(max_events):
+        """
+        Validate max events configuration
+        """
+        if max_events is None:
+            return -1
+        if isinstance(max_events, (int, str)) is False:
+            raise ValueError(
+                "Max events must be a valid integer (or str-representation)."
+            )
+        if max_events < -1 or max_events == 0:
+            raise ValueError(
+                "Max events must be -1 (consider all events) or higher than 0."
+            )
+        return int(max_events)
+
+    @staticmethod
+    def __resolve_considered_events(max_events, n_events):
+        """
+        Resolve number of events considered in event loop
+        """
+        if max_events == -1 or max_events > n_events:
+            return n_events
+        return max_events
+
     def start(self) -> None:
         """
         Start event loop
         """
-        dataset = self.provider.read_file(self.config.get("file_path"))
-        events, _ = self.provider.parse_events(dataset, objects=self.config.get("objects"))
         self.evt_handler.set_config(self.config)
+        max_events = self.__validate_max_entries(self.config.get("max_events"))
+
+        dataset = self.provider.read_file(self.config.get("file_path"))
+        events, n_events = self.provider.parse_events(
+            dataset, objects=self.config.get("objects")
+        )
+        considered_events = self.__resolve_considered_events(max_events, n_events)
 
         if HAS_TQDM:
-            events = tqdm.tqdm(events)
+            events = tqdm.tqdm(events, total=considered_events)
 
-        for event in events:
+        for evt_number, event in enumerate(events, start=1):
+
+            # Control number of considered events chosen by user
+            if evt_number > considered_events:
+                break
+
             # Parsing <Record ...> as Dict[] is much more performatic to the Event Loop
             event = {k: event.__getattr__(k) for k in self.config.get("objects")}
             obj = self.evt_handler.main(event)
